a99d4b7a327abc0153a9af1c5f561d43a5b042f8
larafazani <laraegreja@gmail.com>
2026-06-17 19:58:26 -0300

    Update jogodavelha.c

--- jogodavelha.c
+++ jogodavelha.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "function_jogodavelha.c"
+#include "functions_jogodavelha.c"
 
 void inicializar_tabuleiro(char tab[3][3]);
 void exibir_tabuleiro(char tab[3][3]);
